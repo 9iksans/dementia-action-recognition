5c194a10e3cea35eb985de07327c9295b76e58df
unknown <luqmanhakimiksan@gmail.com>
2020-09-24 15:13:14 +0700

    multiperson data succsessful detection, but in manual parsing

--- skeleton-recognition.py
+++ skeleton-recognition.py
@@ -126,7 +126,7 @@ while(True):
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+#cek
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
